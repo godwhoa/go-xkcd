0ebbe0e59098eb4fd7a881538c4e8dc06a240ca2
godwhoa <gjdaniel1999@gmail.com>
2014-11-12 19:59:47 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"github.com/rkoesters/xkcd"
+)
+
+func main() {
+	//Get the comic data
+	comic, err := xkcd.GetCurrent()
+	perror(err)
+
+	m := martini.Classic()
+	//Includes static files(css,img,js etc.)
+	m.Use(martini.Static("static"))
+	//Renderes the templates
+	m.Use(render.Renderer())
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", comic)
+	})
+	m.Run()
+}
+
+//Error handling
+func perror(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
